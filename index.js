784d454f0e7611397d8835e806df36d989c3b538
AgMac <angelia.gyq@gmail.com>
2018-01-22 12:44:21 -0500

    blank project

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import './App/Config/ReactotronConfig'
-import { AppRegistry } from 'react-native'
-import App from './App/Containers/App'
+import { AppRegistry } from 'react-native';
+import App from './App';
 
-AppRegistry.registerComponent('EduAce', () => App)
+AppRegistry.registerComponent('EA', () => App);
