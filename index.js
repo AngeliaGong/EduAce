7590f890fea294220b962a095e7c626a0039f8af
AgMac <angelia.gyq@gmail.com>
2018-01-23 01:19:29 -0500

    login page UI half done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src/login/App';
 
 AppRegistry.registerComponent('EA', () => App);
---
+++ src/login/App.js
@@ -0,0 +1,36 @@
+/**
+ * EduAce App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View
+} from 'react-native';
+import Logo from './Logo'
+import Form from './Form'
+import SignupSection from './SignupSection'
+import ButtonSubmit from './ButtonSubmit.js'
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Logo />
+        <Form />
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#5AC5E5',
+  },
+});
